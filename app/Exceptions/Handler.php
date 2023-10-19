744449903a3ac2de0b9a31129e402f5eb451925b
Selim Reza <cseselimreza@gmail.com>
2023-10-20 00:39:26 +0600

    student migration add

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Exceptions;
+
+use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array<int, class-string<Throwable>>
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array<int, string>
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
+
+        $this->renderable(function (ModelNotFoundException $e, $request) {
+            if ($request->wantsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'Item Not Found'], 404);
+            }
+        });
+
+        $this->renderable(function (AuthenticationException $e, $request) {
+            if ($request->wantsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'unAuthenticated'], 401);
+            }
+        });
+        $this->renderable(function (ValidationException $e, $request) {
+            if ($request->wantsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'UnprocessableEntity', 'errors' => []], 422);
+            }
+        });
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            if ($request->wantsJson() || $request->is('api/*')) {
+                return response()->json(['message' => 'The requested link does not exist'], 400);
+            }
+        });
+    }
+}
---
+++ database/migrations/2023_10_19_172105_create_student_profiles_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('student_name',100);
+            $table->string('father_name',100);
+            $table->string('father_nid_no',50)->nullable();
+            $table->string('father_occupation',50)->nullable();
+            $table->string('father_contact',50)->nullable();
+            $table->string('occupation_category',50)->nullable();
+            $table->string('unit',255)->nullable();
+            $table->string('rank',255)->nullable();
+            $table->string('mother_name',100)->nullable();
+            $table->string('mother_contact',50)->nullable();
+            $table->text('permanent_address')->nullable();
+            $table->string('date_of_birth',100);
+            $table->string('birth_registration_no',50)->nullable();
+            $table->tinyInteger('student_sex')->comment('1=male,2=famale,3=other');
+            $table->string('religion',50);
+            $table->string('last_school_name',100)->nullable();
+            $table->string('last_exam',100)->nullable();
+            $table->string('last_exam_roll',50)->nullable();
+            $table->string('last_exam_result',50)->nullable();
+            $table->string('blood_group',50)->nullable();
+            $table->string('height',255)->nullable();
+            $table->string('legal_guardian_name',255)->nullable();
+            $table->string('relation_with_guardian',100)->nullable();
+            $table->string('guardian_occupation',255)->nullable();
+            $table->decimal('guardian_income',2)->nullable();
+            $table->text('skill')->nullable();
+            $table->string('profile_image',255)->nullable();
+            $table->string('birth_certificate',255)->nullable();
+            $table->string('unit_certificate',255)->nullable();
+            $table->tinyInteger('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_profiles');
+    }
+}
